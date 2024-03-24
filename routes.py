181cf6eae556afca5f786bef292c8df334b6e736
JESchoett <janeric.schoett@gmail.com>
2024-03-24 14:35:32 +0100

    implemented a book details site with the option to delete an entry

--- routes.py
+++ routes.py
@@ -32,6 +32,20 @@ def register_routes(app, db):
             all_books = Book.query.all()
             return render_template("book_overview.html", all_books=all_books)
 
+    def changeBookDataOverForm(book_form, db):
+        book = Book(autor=book_form.autor.data, titel=book_form.titel.data, genre=book_form.genre.data,
+            subgenreRomane=book_form.subgenreRomane.data, format=book_form.format.data,
+            verlag=book_form.verlag.data, laenge=book_form.laenge.data, isbn=book_form.isbn.data,
+            jahr=book_form.jahr.data, preis=book_form.preis.data, standort=book_form.standort.data,
+            inhaltsangabe=book_form.inhaltsangabe.data, bemerkungen=book_form.bemerkungen.data,
+            subtitel=book_form.subtitel.data, subgenreSachbuchRatgeber=book_form.subgenreSachbuchRatgeber.data,
+            subgenreRatgeber=book_form.subgenreRatgeber.data, auflage=book_form.auflage.data,
+            schlagw=book_form.schlagw.data, bild=book_form.bild.data)
+
+        db.session.add(book)
+        db.session.commit()
+
+
     @app.route('/book_add', methods=['GET', 'POST'])
     def book_add():
         book_form = BookForm()
@@ -39,14 +53,21 @@ def register_routes(app, db):
             all_books = Book.query.all()
             return render_template("book_add.html", all_books=all_books, form=book_form)
         elif request.method == 'POST':
-            book = Book(Autor = book_form.autor.data, Titel = book_form.titel.data, Genre = book_form.genre.data, SubgenreRomane = book_form.subgenre_romane.data,
-                        Format = book_form.format.data, Verlag = book_form.verlag.data, Laenge = book_form.laenge.data,
-                        Seiten = book_form.seiten.data, ISBN = book_form.isbn.data, Jahr = book_form.jahr.data,
-                        Preis = book_form.preis.data, Standort = book_form.standort.data, Inhaltsangabe = book_form.inhaltsangabe.data,
-                        Bemerkungen = book_form.bemerkungen.data, Subtitel = book_form.subtitel.data,
-                        SubgenreSachbuchRatgeber = book_form.subgenre_sachbuch_ratgeber.data, SubgenreRatgeber = book_form.subgenre_ratgeber.data,
-                        Auflage = book_form.auflage.data, Schlagw = book_form.schlagw.data, Bild = book_form.bild.data)
-
-            db.session.add(book)
-            db.session.commit()
+            changeBookDataOverForm(book_form=book_form, db=db)
             return render_template('index.html')
+
+    @app.route('/book_details/<nummer>')
+    def book_details(nummer):
+        book = Book.query.filter(Book.nummer == nummer).first()
+        book_form = BookForm(obj=book)
+        if request.method == 'GET':
+            return render_template('book_details.html', book=book, form=book_form)
+        elif request.method == 'POST':
+            changeBookDataOverForm(book_form=book_form, db=db)
+            return render_template('index.html')
+
+    @app.route('/book_delete/<nummer>', methods=['DELETE'])
+    def book_delete(nummer):
+        Book.query.filter(Book.nummer == nummer).delete()
+        db.session.commit()
+        return render_template('index.html')

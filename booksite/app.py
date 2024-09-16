abd771c8744ed38cd4d3cd2cf3a9b9782f9bc72a
JESchoett <janeric.schoett@gmail.com>
2024-09-16 13:18:47 +0200

    read the secretkey from the .env

--- booksite/app.py
+++ booksite/app.py
@@ -24,15 +24,17 @@ csrf = CSRFProtect()
 def create_app():
     app = Flask(__name__, template_folder='templates')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/buchsammlung.db'
+    #current_dir = os.path.dirname(os.path.abspath('buchsammlung.db'))
+    #database_file = os.path.join(current_dir, 'instance', 'buchsammlung.db')
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_file
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buchsammlung.db'# + database_file
 
     #get the Sekret key from a .env file
     load_dotenv()
     app.secret_key = os.getenv('APP_SECRET_KEY')
-
-    #test for csrf
-    app.config['CSRF_SECRET_KEY'] = 'test for csrf'
-    app.config['WTF_CSRF_SECRET_KEY'] = 'test for csrf'
+    app.config['CSRF_SECRET_KEY'] = os.getenv('CSRF_SECRET_KEY')
+    app.config['WTF_CSRF_SECRET_KEY'] = os.getenv('WTF_CSRF_SECRET_KEY')
 
     #init the Database to use it in the app
     db.init_app(app)
@@ -62,17 +64,14 @@ def create_app():
     from booksite.user_handeling.routes import user_handeling
 
     app.register_blueprint(core, url_prefix='/')
+    app.register_blueprint(user_handeling, url_prefix='/user_handeling', bcrypt=bcrypt)
     app.register_blueprint(books, url_prefix='/books')
     app.register_blueprint(movies, url_prefix='/movies')
-    app.register_blueprint(user_handeling, url_prefix='/user_handeling', bcrypt=bcrypt)
 
     csrf.exempt(core)
+    csrf.exempt(user_handeling)
     csrf.exempt(books)
     csrf.exempt(movies)
-    csrf.exempt(user_handeling)
 
     migrate = Migrate(app, db)
     return app
-
-
-
--- sql/create_user.sql
+++ sql/create_user.sql
@@ -1,4 +1,4 @@
-create table if not exists user (
+create table if not exists users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL

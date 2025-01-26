ca1071ad3c5117ed456f388647b2801fbc47cec6
JESchoett <janeric.schoett@gmail.com>
2025-01-26 22:35:24 +0100

    Adjusted the run function to handel static files

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from booksite.app import create_app
 
+from flask import Flask, send_from_directory
 flask_app = create_app("development")
+
+#allow the handeling of static files in the /booksite/static folder
+@flask_app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
+
 
 if __name__ == "__main__":
     flask_app.run(host='0.0.0.0', debug=True, port=5003)

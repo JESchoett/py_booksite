fb1e57ffe704f0f860a40bd2fd98ea3456c1ebd1
SvenjaY <62055205+SvenjaY@users.noreply.github.com>
2025-07-07 22:50:24 +0200

    Styling
    Styled the book_add page. Translated a lot of the terms to German. Made the book overview responsive.

--- booksite/user_handeling/loginForm.py
+++ booksite/user_handeling/loginForm.py
@@ -3,6 +3,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField(label='Username', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label="Log In")
+    username = StringField(label='Nutzername', validators=[DataRequired()])
+    password = PasswordField(label='Passwort', validators=[DataRequired()])
+    submit = SubmitField(label="Einloggen")

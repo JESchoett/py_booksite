152bfb0fb923540be07d86f0378ae6078bf0eef0
SvenjaY <62055205+SvenjaY@users.noreply.github.com>
2025-07-12 21:34:35 +0200

    Styling and translation
    book add popup and details

--- booksite/user_handeling/signupForm.py
+++ booksite/user_handeling/signupForm.py
@@ -3,6 +3,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class SignupForm(FlaskForm):
-    username = StringField(label='Username', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label="Signup")
+    username = StringField(label='Nutzername', validators=[DataRequired()])
+    password = PasswordField(label='Passwort', validators=[DataRequired()])
+    submit = SubmitField(label="Registrieren")
